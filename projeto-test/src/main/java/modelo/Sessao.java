a09e267683299c2be67fd5849f313da51bd29555
Vitor Hugo Schiphorst <vitor.s13@aluno.ifsc.edu.br>
2023-08-08 11:57:03 -0300

    Aula 2 Atualizado 2.0

--- projeto-test/src/main/java/modelo/Sessao.java
+++ projeto-test/src/main/java/modelo/Sessao.java
@@ -1,5 +1,19 @@
 package modelo;
 
 public class Sessao {
-
+	private int idSessao;
+	private String nomeSessao;
+	
+	public int getIdSessao() {
+		return idSessao;
+	}
+	public void setIdSessao(int idSessao) {
+		this.idSessao = idSessao;
+	}
+	public String getNomeSessao() {
+		return nomeSessao;
+	}
+	public void setNomeSessao(String nomeSessao) {
+		this.nomeSessao = nomeSessao;
+	}
 }
--- projeto-test/src/main/java/visao/TelaCadastro.java
+++ projeto-test/src/main/java/visao/TelaCadastro.java
@@ -5,10 +5,17 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JTextField;
+import javax.swing.JLabel;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 public class TelaCadastro extends JFrame {
 
 	private JPanel contentPane;
+	private JTextField textIdSessao;
+	private JTextField textNomeSessao;
 
 	/**
 	 * Launch the application.
@@ -36,6 +43,37 @@ public class TelaCadastro extends JFrame {
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		textIdSessao = new JTextField();
+		textIdSessao.setBounds(101, 23, 86, 20);
+		contentPane.add(textIdSessao);
+		textIdSessao.setColumns(10);
+		
+		textNomeSessao = new JTextField();
+		textNomeSessao.setBounds(101, 66, 86, 20);
+		contentPane.add(textNomeSessao);
+		textNomeSessao.setColumns(10);
+		
+		JLabel lblNewLabel = new JLabel("ID Sessão:");
+		lblNewLabel.setBounds(28, 26, 63, 14);
+		contentPane.add(lblNewLabel);
+		
+		JLabel lblNewLabel_1 = new JLabel("Nome da Sessão:");
+		lblNewLabel_1.setBounds(10, 69, 107, 14);
+		contentPane.add(lblNewLabel_1);
+		
+		JButton btnNewButton = new JButton("Cadastrar");
+		btnNewButton.setBounds(39, 133, 89, 23);
+		contentPane.add(btnNewButton);
+		
+		btnNewButton.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Integer idSessão = Integer.valueOf(textIdSessao.getText());
+				String nomeSessao = textNomeSessao.getText();
+			}
+		});
+		
+		
 	}
-
 }
